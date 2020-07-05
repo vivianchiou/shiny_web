c6e203c0a7adb67356b457e096acb47bbc7b3d46
陳冠中 <56258159+TanerYee@users.noreply.github.com>
2020-07-05 12:07:55 +0800

    relation plot

--- shiny_v2/app.R
+++ shiny_v2/app.R
@@ -26,6 +26,9 @@ library(maptools)
 library(rgeos)
 library(sf)
 library(DT)
+library(edgebundleR)
+library(htmlwidgets)
+library(igraph)
 coords2continent = function(points)
 {  
   countriesSP <- getMap(resolution='low')
@@ -70,7 +73,9 @@ city_name_convert_df <- data.frame(city_name_ch = c("台北市", "新北市", "�
 # load kaggle data
 raw_data <- read.csv("data/covid_19_data.csv", stringsAsFactors = FALSE)
 # raw_data <- read.csv("shiny_v2/data/covid_19_data.csv", stringsAsFactors = FALSE) # local check
-
+relate_df <- readxl::read_excel(path ='data/case_relation_in_waiwan.xlsx')
+# relate_df <- readxl::read_excel(path ='C:\\Users\\user\\Documents\\GitHub\\my_code\\shiny_web\\case_relation_in_waiwan.xlsx')
+detail_cases <- readxl::read_excel(path ='data/part_detail_cases.xlsx')
 #2-1======================================================================================
 #< data clean > - world data =============================================================
 data_date <- df$date %>% unique()
@@ -243,6 +248,19 @@ date_total <- covid_19_data %>%
 
 date_list = covid_19_data$date %>% unique()
 country_list = covid_19_data$country_region %>% unique()
+#2-4======================================================================================
+#< data clean > - cases relation =========================================================
+detail_cases <- detail_cases %>% 
+  mutate(select_gender = ifelse(detail_cases$gender=="女性", "F", "M"),
+         select_source = ifelse(detail_cases$imported=="本土", "L", ifelse(detail_cases$imported=="境外", "I", "S")))
+# nodes <- data.frame(case = 1:nrow(detail_cases))
+#View(detail_cases)
+x <- graph.data.frame(relate_df , directed = F, vertices = detail_cases)
+clr <- as.factor(V(x)$select_source)
+levels(clr) <- c("cyan5", "red", "darkorange")# 1L I S
+V(x)$color <- as.character(clr)
+#p <- edgebundle(x, tension = 0.5,fontsize = 4, nodesize = c(1, 1), width = NULL, padding = 50, directed = F, cutoff = 0.5)
+#saveWidget(p, file="plot_edgebundle.html")
 #3========================================================================================
 #< ui >===================================================================================
 ui <- navbarPage(title = "Covid-19 dashboard!",
@@ -340,7 +358,21 @@ ui <- navbarPage(title = "Covid-19 dashboard!",
                               DTOutput('intro_data')
                           )
                           
-                 )
+                 ),
+                 navbarMenu(title = 'reports',
+                            tabPanel(title = 'cases relation_TW',
+                                     sidebarPanel(width = 2,
+                                                  title = 'control par',
+                                                  sliderInput("tension", "Tension", 0.5,min=0,max=1,step = 0.01),
+                                                  sliderInput("fontsize","Font size",12,min=2,max=10),
+                                                  sliderInput("width","Width and height",1200,min=200,max=1200),
+                                                  sliderInput("padding","Padding",100,min=0,max=300)
+                                                  ),
+                                     box(width = 10,solidHeader = TRUE,verbatimTextOutput("type"),
+                                         uiOutput("circplot"))
+                                     )
+                            )
+                 
 )
 #shinyApp(ui,server)
 #4========================================================================================
@@ -614,6 +646,18 @@ server <- function(input, output, session) {
     DT::datatable(dt, options = list(
       pageLength = 25
     ))
+  })
+  
+  output$circplot <- renderUI({
+    edgebundleOutput("eb", width = input$width, height=input$width)
+  })
+  
+  # output$type=reactive({V(a)$select_source})
+  # outputOptions(output, 'type', suspendWhenHidden=FALSE)
+  
+  output$eb <- renderEdgebundle({
+    edgebundle(x,tension=input$tension,cutoff=input$cutoff,
+               fontsize=input$fontsize,padding=input$padding)
   })
 }
 shinyApp(ui,server)
